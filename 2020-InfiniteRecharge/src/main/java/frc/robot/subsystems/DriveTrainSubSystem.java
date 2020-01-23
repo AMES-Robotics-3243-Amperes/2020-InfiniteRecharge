9a3cb5a7658488b6a962f8ec60f08efde00f8a64
LeagueOfFreeWorlds <inathan2347@gmail.com>
2020-01-23 16:39:03 -0700

    new drive sub

---
+++ 2020-InfiniteRecharge/src/main/java/frc/robot/subsystems/DriveTrainSubSystem.java
@@ -0,0 +1,29 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/**
+ * Add your docs here.
+ */
+public class DriveTrainSubSystem {
+    private CANSparkMax motor_1 = new CANSparkMax(0, MotorType.kBrushless);
+    private CANSparkMax motor_2 = new CANSparkMax(1, MotorType.kBrushless);
+    private CANSparkMax motor_3 = new CANSparkMax(2, MotorType.kBrushless);
+    private CANSparkMax motor_4 = new CANSparkMax(3, MotorType.kBrushless);
+
+    public motors(){
+        motor_1 = set(1);
+        
+    }
+
+
+}
