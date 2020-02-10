ef15e730178ac6aee621b062dd3112a0e566b159
LeagueOfFreeWorlds <inathan2347@gmail.com>
2020-02-10 16:30:33 -0700

    Added 'sentience.java' as a new autonomous command class.

--- 2020-InfiniteRecharge/src/main/java/frc/robot/commands/Sentience.java
+++ 2020-InfiniteRecharge/src/main/java/frc/robot/commands/Sentience.java
@@ -4,41 +4,33 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
+// This is the autonomous class
 package frc.robot.commands;
-
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveTrainSubSystem;
-import frc.robot.RobotContainer;
-
-public class DriveTrainCommand extends CommandBase {
-  
-  private final DriveTrainSubSystem m_drive;  //This class's object of DriveTrainSubSystem
-
-  public DriveTrainCommand(DriveTrainSubSystem drive) { //drive = actual import from DriveTrainSubSystem
-    m_drive = drive;
+public class Sentience extends CommandBase {
+  double m_time;
+  public Sentience(double time) { // Scope has the amount of time we want it to run
+    // Use addRequirements() here to declare subsystem dependencies.
+    m_time = time; 
+    
   }
+
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    
-      DriveTrainSubSystem.tankDrive( 
-        RobotContainer.configureDriveLeft(), 
-        RobotContainer.configureDriveRight(),
-        RobotContainer.configurePracBot() //Get rid of this later on
-      );
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    DriveTrainSubSystem.tankDrive(0.0, 0.0, false); //Get rid of "false" later on
   }
 
   // Returns true when the command should end.
