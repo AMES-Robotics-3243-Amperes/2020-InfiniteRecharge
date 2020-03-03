eec198d4fea5af1cc268bddc16905b0bb5465f32
amesrobotics <ames.amperes@gmail.com>
2020-03-02 18:04:15 -0700

    Limelight aiming progress

--- 2020-InfiniteRecharge/src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ 2020-InfiniteRecharge/src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -72,7 +72,7 @@ public class LimelightSubsystem extends SubsystemBase {
   /** Returns the distance from the front of the robot frame to the face of the high goal */
   public double getInchesFromHighGoal()
   {
-    return (Constants.FieldConstants.highGoalHeightInches - Constants.LimelightConstants.INCHES_TO_GROUND) / (Math.tan(x + Constants.LimelightConstants.PITCH_DEGREES))
+    return (Constants.FieldConstants.highGoalHeightInches - Constants.LimelightConstants.INCHES_TO_GROUND) / (Math.tan(y + Constants.LimelightConstants.PITCH_DEGREES))
       - Constants.LimelightConstants.INCHES_TO_FRAME_FRONT;
   }
 
